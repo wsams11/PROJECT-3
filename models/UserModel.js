8c543047c781e750b1e719cb72874bff31ce2c7b
Larry <richardklevan@gmail.com>
2020-01-03 10:36:02 -0700

    added userModel

---
+++ models/UserModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+
+
+
+
+
+
+    
+  });
+
+const User = mongoose.model("User", Schema);
+
+module.exports = User;
