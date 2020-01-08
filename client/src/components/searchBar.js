297d410ad85a7c08815f5ce599c5feb86666751b
Jacob Lara <l1a9r9a7@gmail.com>
2020-01-08 10:31:46 -0700

    updates to NYT api call

--- client/src/components/searchBar.js
+++ client/src/components/searchBar.js
@@ -17,7 +17,7 @@ class SearchBar extends Component {
       <div className="search-bar">
         <input
           value={this.state.term}
-          onClick={(event) => {
+          onChange={(event) => {
             this.onInputChange(event.target.value)
           }}
           placeholder='Search Video'
