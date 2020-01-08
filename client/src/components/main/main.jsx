be044de1bcde03af7e33e8bad4122619c6d97d39
Jacob Lara <l1a9r9a7@gmail.com>
2020-01-08 10:40:06 -0700

    updates to NYT api call and changes page varialbe names

--- client/src/components/main/main.jsx
+++ client/src/components/main/main.jsx
@@ -3,28 +3,28 @@ import "./style.css";
 
 const Main = (props) => {
   return (
-    <body class="text-center">
-        <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-      <header class="masthead mb-auto">
+    <body className="text-center">
+        <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header className="masthead mb-auto">
         <div class="inner">
-          <nav class="nav nav-masthead justify-content-center">
-            <a class="nav-link active" href="#">Home</a>
-            <a class="nav-link" href="../topicspage/topics.html">Topics</a>
-            <a class="nav-link" href="#">Login / Sign Up</a>
+          <nav className="nav nav-masthead justify-content-center">
+            <a className="nav-link active" href="#">Home</a>
+            <a className="nav-link" href="../topicspage/topics.html">Topics</a>
+            <a className="nav-link" href="#">Login / Sign Up</a>
           </nav>
         </div>
       </header>
     
-      <main role="main" class="inner cover">
-        <h1 class="cover-heading">TRANSITIONS</h1>
-        <p class="lead">The helpful guide to becoming an effective adult. </p>
-        <p class="lead">
-          <a href="#" class="btn btn-lg btn-secondary" style="background-color:transparent">Sign Up!</a>
+      <main role="main" className="inner cover">
+        <h1 className="cover-heading">TRANSITIONS</h1>
+        <p className="lead">The helpful guide to becoming an effective adult. </p>
+        <p className="lead">
+          <a href="#" className="btn btn-lg btn-secondary" style="background-color:transparent">Sign Up!</a>
         </p>
       </main>
     
-      <footer class="mastfoot mt-auto">
-        <div class="inner">
+      <footer className="mastfoot mt-auto">
+        <div className="inner">
           <p>App Created by Jacob Lara, Richard K, William Sams III</p>
         </div>
       </footer>
