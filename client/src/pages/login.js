088feec711cdb19a43cb98d9b42258eadd10a5c4
Larry <richardklevan@gmail.com>
2020-01-10 12:54:38 -0700

    fixed Signup functionality

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -2,176 +2,169 @@
 import React, { useState, useEffect } from 'react';
 import NavBar from "../components/navBar";
 import axios from "axios";
-// import Footer from '../components/footer'
-import { Container, Form, Button, Col, Row, DropdownButton, Dropdown } from "react-bootstrap";
+import Footer from '../components/footer'
+import { Jumbotron, Container, Form, Button, Col, Row, DropdownButton, Dropdown } from "react-bootstrap";
+import UserContext from "../userContext.js";
+// import {setUser, user} from "../App.js"
 
-import "../components/main/cover.css"
+// import "./login.css"
 // import "bootstrap/dist/css/bootstrap.min.css";
 
 const topicsArray = ["Taxes", "Finances", "Renting an Apartment", "Buying a Home", "Saving and Investing", "Buying a Car", "Career Advice", "Investing", "Insurance"];
 
-const Login = (props) => {
+function Login(props) {
+  const { setTopic, handleChange, handleSignupSubmit, handleLoginSubmit } = props;
+  console.log(props)
+  // const {email, password, topics } = UseContext(UserContext);
 
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
-    topics: ""
-  })
+  // const handleChange = e => {
+  //   const { name, value } = e.target;
+  //   setUser({
+  //     ...user,
+  //     [name]: value
+  //   });
+  // }
 
-  const handleChange = e => {
-    const { name, value } = e.target;
-    setUser({
-      ...user,
-      [name]: value
-    });
-  }
+  // const setTopic = topic => {
+  //   setUser({
+  //     ...user,
+  //     topics: topic
+  //   });
+  // }
 
-  const setTopic = topic => {
-    setUser({
-      ...user,
-      topics: topic
-    });
-  }
+  // const handleLoginSubmit = e => {
+  //   e.preventDefault();
+  //   console.log("handleSubmit")
 
-  const handleLoginSubmit = e => {
-    e.preventDefault();
-    console.log("handleSubmit")
+  //   axios.post("v1/user/signin", {
+  //     email: user.email,
+  //     password: user.password,
+  //     topics: user.topics
+  //   })
+  //     .then(response => {
+  //       console.log("login response: ");
+  //       console.log(response);
+  //       if (response.status === 200) {
+  //         localStorage.setItem("token", response.data.token);
+  //         // this.props.updatedUser({
+  //         //   loggedIn: true,
+  //         //   email: response.data.email,
+  //         //   topics: response.data.topics
+  //         // });
+  //         // this.setState({
+  //         //   email: "",
+  //         //   password: "",
+  //         //   topics: "",
+  //         //   redirectTo: ""
+  //         // })
+  //         props.history.push("/home");
+  //       }
+  //     })
+  //     .catch(error => {
+  //       console.log(error);
+  //     })
+  // };
 
-    axios.post("v1/user/signin", {
-      email: user.email,
-      password: user.password,
-      topics: user.topics
-    })
-      .then(response => {
-        console.log("login response: ");
-        console.log(response);
-        if (response.status === 200) {
-          localStorage.setItem("token", response.data.token);
-          this.props.updatedUser({
-            loggedIn: true,
-            email: response.data.email,
-            topics: response.data.topics
-          });
-          // this.setState({
-          //   email: "",
-          //   password: "",
-          //   topics: "",
-          //   redirectTo: ""
-          // })
-          // props.history.push("/home");
-          response.redirect("/home")
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  };
+  // const handleSignupSubmit = e => {
+  //   e.preventDefault();
+  //   console.log("handleSubmit")
 
-  const handleSignupSubmit = e => {
-    e.preventDefault();
-    console.log("handleSubmit")
-
-    axios.post("v1/user/signup", {
-      email: user.email,
-      password: user.password,
-      topics: user.topics
-    })
-      .then(response => {
-        console.log("login response: ");
-        console.log(response);
-        if (response.status === 200) {
-          localStorage.setItem("token", response.data.token);
-          props.updatedUser({
-            loggedIn: true,
-            email: response.data.email,
-            topics: response.data.topics
-          });
-          // this.setState({
-          //   email: "",
-          //   password: "",
-          //   topics: "",
-          //   redirectTo: ""
-          // })
-          props.history.push("/home");
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  };
+  //   axios.post("v1/user/signup", {
+  //     email: user.email,
+  //     password: user.password,
+  //     topics: user.topics
+  //   })
+  //     .then(response => {
+  //       console.log("login response: ");
+  //       console.log(response);
+  //       if (response.status === 200) {
+  //         localStorage.setItem("token", response.data.token);
+  //         props.updatedUser({
+  //           loggedIn: true,
+  //           email: response.data.email,
+  //           topics: response.data.topics
+  //         });
+  //         // this.setState({
+  //         //   email: "",
+  //         //   password: "",
+  //         //   topics: "",
+  //         //   redirectTo: ""
+  //         // })
+  //         props.history.push("/home");
+  //       }
+  //     })
+  //     .catch(error => {
+  //       console.log(error);
+  //     })
+  // };
 
 
 
   return (
 
-    <Container>
-      <header className="masthead mb-auto">
-        <div className="inner">
-          <nav className="nav nav-masthead justify-content-center">
-            <a className="nav-link" href="/">Home</a>
-            <a className="nav-link" href="/topics">Topics</a>
-            <a className="nav-link active" href="/login">Login / Sign Up</a>
-          </nav>
-        </div>
-      </header>
-      <main role="main" className="inner cover">
-        {/* <h1 className="cover-heading">TRANSITIONS</h1> */}
-        <Row className="loginRow">
+    <UserContext.Consumer >
 
-          <Col xs={12} md={6} lg={5} className="LoginContainer">
+      {user =>
+        <Container>
+          <NavBar />
+          <main role="main" className="inner cover">
+            <h1 className="cover-heading">TRANSITIONS</h1>
+            <Row>
 
-            <h1> Login</h1>
+              <Col xs={12} md={6} lg={5} className="LoginContainer">
 
-            <Form className="Form">
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
+                <h1> Login</h1>
+
+                <Form classNanme="Form">
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                      We'll never share your email with anyone else.
                 </Form.Text>
-              </Form.Group>
+                  </Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                  </Form.Group>
 
-              <Button
-                // onClick={handleLoginSubmit} 
-                className="submitBtn" type="submit">
-                Submit
+                  <Button
+                    onClick={handleLoginSubmit}
+                    className="submitBtn" type="submit">
+                    Submit
   `           </Button>
-            </Form>
+                </Form>
 
-          </Col>
+              </Col>
 
-          <Col xs={12} md={6} lg={5} className="SignupContainer">
+              <Col xs={12} md={6} lg={5} className="SignupContainer">
 
-            <h1> Signup</h1>
-            <Form className="Form">
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control onChange={handleChange} value={user.email} name="email" type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
+                <h1> Signup</h1>
+                <Form classNanme="Form">
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control onChange={handleChange} value={user.email} name="email" type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                      We'll never share your email with anyone else.
                 </Form.Text>
-              </Form.Group>
+                  </Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control onChange={handleChange} name="password" value={user.password} type="password" placeholder="Password" />
-              </Form.Group>
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control onChange={handleChange} name="password" value={user.password} type="password" placeholder="Password" />
+                  </Form.Group>
 
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                  </Form.Group>
 
-              <DropdownButton id="dropdownBtn" title={user.topic || "Select Topics"}>
+                  <DropdownButton id="dropdownBtn" title={user.topic || "Select Topics"}>
 
-                {topicsArray.map(topic => <Dropdown.Item onClick={() => setTopic(topic)}>{topic}</Dropdown.Item>)}
-                {/* <Dropdown.Item>Taxes</Dropdown.Item>
+                    {topicsArray.map(topic => <Dropdown.Item onClick={() => setTopic(topic)}>{topic}</Dropdown.Item>)}
+                    {/* <Dropdown.Item>Taxes</Dropdown.Item>
                 <Dropdown.Item>Finances</Dropdown.Item>
                 <Dropdown.Item>Renting an Apartment</Dropdown.Item>
                 <Dropdown.Item>Buying a Home</Dropdown.Item>
@@ -180,19 +173,20 @@ const Login = (props) => {
                 <Dropdown.Item >Career Advice</Dropdown.Item>
                 <Dropdown.Item >Interviewing </Dropdown.Item>
                 <Dropdown.Item >Insurance</Dropdown.Item> */}
-              </DropdownButton>
-              <Button className="submitBtn" onClick={handleSignupSubmit} type="submit">
-                Submit
+                  </DropdownButton>
+                  <Button className="submitBtn" onClick={handleSignupSubmit} type="submit">
+                    Submit
               </Button>
-            </Form>
+                </Form>
 
-          </Col>
-        </Row>
+              </Col>
+            </Row>
 
-      </main>
+          </main>
 
-     
-    </Container>
+          <Footer />
+        </Container>}
+    </UserContext.Consumer>
 
   )
 }
