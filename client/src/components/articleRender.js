a9110a01effcdf62e08ed9341c6a997a0ed25999
William Sams III <w.sams11@yahoo.com>
2020-01-13 08:43:10 -0700

    styling updating on articles

--- client/src/components/articleRender.js
+++ client/src/components/articleRender.js
@@ -7,9 +7,11 @@ export default class ArticlesRender extends React.Component {
         <div className="row">
           {this.props.articles.map((articles, index) => (
             <div className="col-lg-4 " key={index}>
-              <div className="acard-block" style={{ height: '380' }}>
+              <div className="acard-block">
                 <img className="acard-text" src={articles.urlToImage} alt={ "article" } />
+                <div className="row">
                 <a className="acard-footer" href={articles.url} target="_blank" rel="noopener noreferrer">{articles.title}</a>
+              </div>
               </div>
             </div>
           ))
